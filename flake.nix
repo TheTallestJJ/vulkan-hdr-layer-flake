73764696890f6a51d801b61124b14b593bc56fc6
Jeroen Jetten <me@jeroen-jetten.com>
2024-03-01 18:53:06 +0100

    add dots

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, ... }: {
     packages = {
       vulkan-hdr-layer = nixpkgs.legacyPackages.${nixpkgs.system or "x86_64-linux"}.vulkan-hdr-layer;
     };
